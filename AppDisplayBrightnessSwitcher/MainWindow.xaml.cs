6b8de8f13daa2ebb593151c8a0627d9218b3b0ac
edza <edexdev@gmail.com>
2015-01-15 10:52:27 +0200

    actual brightness changes, noit just gamma

--- AppDisplayBrightnessSwitcher/MainWindow.xaml.cs
+++ AppDisplayBrightnessSwitcher/MainWindow.xaml.cs
@@ -77,6 +77,33 @@ namespace AppDisplayBrightnessSwitcher
             Brightness.SetBrightness(short.Parse(tb.Text));
         }
 
+        static byte GetBrightness()
+        {
+            //define scope (namespace)
+            System.Management.ManagementScope s = new System.Management.ManagementScope("root\\WMI");
+
+            //define query
+            System.Management.SelectQuery q = new System.Management.SelectQuery("WmiMonitorBrightness");
+
+            //output current brightness
+            System.Management.ManagementObjectSearcher mos = new System.Management.ManagementObjectSearcher(s, q);
+
+            System.Management.ManagementObjectCollection moc = mos.Get();
+
+            //store result
+            byte curBrightness = 0;
+            foreach (System.Management.ManagementObject o in moc)
+            {
+                curBrightness = (byte)o.GetPropertyValue("CurrentBrightness");
+                break; //only work on the first object
+            }
+
+            moc.Dispose();
+            mos.Dispose();
+
+            return curBrightness;
+        }
+
         void ControlBrightness(string title)
         {
             if (!title.Contains("devenv")) // we are in visual studio
@@ -84,7 +111,10 @@ namespace AppDisplayBrightnessSwitcher
                 if (br == 122)
                     return;
 
+                byte b = GetBrightness();
+
                  Brightness.SetBrightness(122);
+                SetBrightness((byte)Math.Min(100, (int)b - 9));
                  br = 122;
             }
             else
@@ -92,9 +122,73 @@ namespace AppDisplayBrightnessSwitcher
                 if (br == 200)
                     return;
 
-                Brightness.SetBrightness(200);
+                byte b = GetBrightness();
+
+                SetBrightness((byte)Math.Max(0, (int)b + 9));
+                Brightness.SetBrightness(180);
                 br = 200;
             }
+        }
+
+        static byte[] GetBrightnessLevels()
+        {
+            //define scope (namespace)
+            System.Management.ManagementScope s = new System.Management.ManagementScope("root\\WMI");
+
+            //define query
+            System.Management.SelectQuery q = new System.Management.SelectQuery("WmiMonitorBrightness");
+
+            //output current brightness
+            System.Management.ManagementObjectSearcher mos = new System.Management.ManagementObjectSearcher(s, q);
+            byte[] BrightnessLevels = new byte[0];
+
+            try
+            {
+                System.Management.ManagementObjectCollection moc = mos.Get();
+
+                //store result
+
+
+                foreach (System.Management.ManagementObject o in moc)
+                {
+                    BrightnessLevels = (byte[])o.GetPropertyValue("Level");
+                    break; //only work on the first object
+                }
+
+                moc.Dispose();
+                mos.Dispose();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Sorry, Your System does not support this brightness control...");
+
+            }
+
+            return BrightnessLevels;
+        }
+
+        static void SetBrightness(byte targetBrightness)
+        {
+            //define scope (namespace)
+            System.Management.ManagementScope s = new System.Management.ManagementScope("root\\WMI");
+
+            //define query
+            System.Management.SelectQuery q = new System.Management.SelectQuery("WmiMonitorBrightnessMethods");
+
+            //output current brightness
+            System.Management.ManagementObjectSearcher mos = new System.Management.ManagementObjectSearcher(s, q);
+
+            System.Management.ManagementObjectCollection moc = mos.Get();
+
+            foreach (System.Management.ManagementObject o in moc)
+            {
+                o.InvokeMethod("WmiSetBrightness", new Object[] { UInt32.MaxValue, targetBrightness }); //note the reversed order - won't work otherwise!
+                break; //only work on the first object
+            }
+
+            moc.Dispose();
+            mos.Dispose();
         }
     }
 }
